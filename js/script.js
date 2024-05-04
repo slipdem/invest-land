74e1a46495675147a0d842711fe88e1b82a0ee9e
Dmytro Slipchenko <slipdem@users.noreply.github.com>
2024-05-04 16:09:37 +0300

    feat: add sliders functionality, add favicon

--- js/script.js
+++ js/script.js
@@ -18,22 +18,29 @@ function handleSubmit(e) {
 	form.reset();
 }
 
-// range input background and calculation change
-function setInputBackground() {
-	const min = this.min;
-	const max = this.max;
-	const currentVal = this.value;
+investRangeInput.addEventListener('input', () => {
+	const min = investRangeInput.min;
+	const max = investRangeInput.max;
+	const currentVal = investRangeInput.value;
 
 	investSliderValue.textContent = currentVal;
-	profitSliderValue.textContent = this.value * 7.5;
-	// profitRangeInput.value = currentVal;
+	profitSliderValue.textContent = currentVal * 7.5;
 
-	return (this.style.backgroundSize =
-		((currentVal - min) / (max - min)) * 100 + '% 100%');
-}
+	investRangeInput.style.backgroundSize =
+		((currentVal - min) / (max - min)) * 100 + '% 100%';
+});
 
-investRangeInput.addEventListener('input', setInputBackground);
-profitRangeInput.addEventListener('input', setInputBackground);
+profitRangeInput.addEventListener('input', () => {
+	const min = profitRangeInput.min;
+	const max = profitRangeInput.max;
+	const currentVal = profitRangeInput.value;
+
+	profitSliderValue.textContent = currentVal;
+	investSliderValue.textContent = Math.ceil(currentVal / 7.5);
+
+	profitRangeInput.style.backgroundSize =
+		((currentVal - min) / (max - min)) * 100 + '% 100%';
+});
 
 // benefits mobile carousel
 
@@ -41,7 +48,7 @@ let carousel = document.querySelector('.benefits__items');
 let items = carousel.querySelectorAll('.benefits__item');
 let dotsContainer = document.querySelector('.dots');
 
-// Insert dots into the DOM
+// insert dots into the DOM
 items.forEach((_, index) => {
 	let dot = document.createElement('span');
 	dot.classList.add('dot');
@@ -52,7 +59,7 @@ items.forEach((_, index) => {
 
 let dots = document.querySelectorAll('.dot');
 
-// Function to show a specific item
+// function to show a specific item
 function showItem(index) {
 	items.forEach((item, idx) => {
 		item.classList.remove('active');
@@ -64,7 +71,7 @@ function showItem(index) {
 	});
 }
 
-// Event listeners for dots
+// event listeners for dots
 dots.forEach((dot) => {
 	dot.addEventListener('click', () => {
 		let index = parseInt(dot.dataset.index);

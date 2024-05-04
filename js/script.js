3360c6154d0e50d1515af3b3d8ac624fa3372deb
Dmytro Slipchenko <slipdem@users.noreply.github.com>
2024-05-04 15:14:20 +0300

    feat: add mobile version

--- js/script.js
+++ js/script.js
@@ -34,3 +34,40 @@ function setInputBackground() {
 
 investRangeInput.addEventListener('input', setInputBackground);
 profitRangeInput.addEventListener('input', setInputBackground);
+
+// benefits mobile carousel
+
+let carousel = document.querySelector('.benefits__items');
+let items = carousel.querySelectorAll('.benefits__item');
+let dotsContainer = document.querySelector('.dots');
+
+// Insert dots into the DOM
+items.forEach((_, index) => {
+	let dot = document.createElement('span');
+	dot.classList.add('dot');
+	if (index === 0) dot.classList.add('active');
+	dot.dataset.index = index;
+	dotsContainer.appendChild(dot);
+});
+
+let dots = document.querySelectorAll('.dot');
+
+// Function to show a specific item
+function showItem(index) {
+	items.forEach((item, idx) => {
+		item.classList.remove('active');
+		dots[idx].classList.remove('active');
+		if (idx === index) {
+			item.classList.add('active');
+			dots[idx].classList.add('active');
+		}
+	});
+}
+
+// Event listeners for dots
+dots.forEach((dot) => {
+	dot.addEventListener('click', () => {
+		let index = parseInt(dot.dataset.index);
+		showItem(index);
+	});
+});
